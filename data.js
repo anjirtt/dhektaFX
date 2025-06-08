f0a6719c84149476c268a39c884dfcb2420837bc
anjirtt <sempakloer52@gmail.com>
2025-06-08 12:00:14 +0700

    Update tokens

--- data.js
+++ data.js
@@ -1,9 +1,5 @@
 {
   "tokens": [
-    "8195907276",
-    "8084971457:AAE0M0zmgXMpbfTep2ZRfxYvEaCUDMw9eTw",
-    "8112323956:AAGIMdBWzMQRvbegFTvc4wdPatygXkySq18",
-    "7264730931:AAGArIh8nIEtWaanYw9isvy1cdez4LSa_LI",
     "8116514559"
   ]
 }
